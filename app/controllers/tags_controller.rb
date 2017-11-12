a30779798d2007664e8a79dde226b78304e503fe
Nolan Neeley <nolan@artmill.com>
2017-11-12 12:38:43 -0600

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,19 @@
+class TagsController < ApplicationController
+
+  def index
+    @tag = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id]).destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to @tag
+  end
+
+end
